c89ccf7c60b03be809d4eac3427fdc69728aac46
Tom Collins <omalleh@gmail.com>
2022-12-12 16:27:19 +0000

    Update keymap.cpp

--- contra-default-single-nice_nano/keymap.cpp
+++ contra-default-single-nice_nano/keymap.cpp
@@ -19,7 +19,7 @@ LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR P
 #include "keymap.h"
 #include "RotaryEncoder.h"
  
-std::array<std::array<Key, MATRIX_ROWS>, MATRIX_COLS> matrix =
+std::array<std::array<Key, MATRIX_COLS>, MATRIX_ROWS> matrix =
     {{
         {KC_ESC,  KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,}, 
         {KC_TAB,  KC_A,    KC_S,    KC_D,   KC_F,    KC_G,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,},
