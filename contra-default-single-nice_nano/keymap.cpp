dcb9ac7b0d4dd6e95fde3e62107d91e1e31d3b92
Tom Collins <omalleh@gmail.com>
2022-12-15 11:03:33 +0000

    Update keymap.cpp

--- contra-default-single-nice_nano/keymap.cpp
+++ contra-default-single-nice_nano/keymap.cpp
@@ -20,12 +20,12 @@ LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR P
 #include "RotaryEncoder.h"
  
 std::array<std::array<Key, MATRIX_ROWS>, MATRIX_COLS> matrix =
-    {{
-        {KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,}, 
-        {KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,},
-        {KC_LSFT, KC_SLSH, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_RSFT,},
-        {KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,  LAYER_1, KC_SPC,  KC_RALT, KC_RGUI, KC_RCTL, KC_MPLY, KC_MUTE}
-    }};
+    {KEYMAP(
+        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+        KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+        KC_LSFT, KC_SLSH, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_RSFT,
+        KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,  LAYER_1, KC_SPC,  KC_RALT, KC_RGUI, KC_RCTL, KC_MPLY, KC_MUTE
+     )};
  
 void setupKeymap() {
 
